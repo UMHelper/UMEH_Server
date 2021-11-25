b15a613d0344e47ee6373659a18eca2ec37af7c4
boxmars <2803726925@qq.com>
2021-11-25 10:55:43 +0800

    update

---
+++ src/main/java/top/umeh/ServletInitializer.java
@@ -0,0 +1,13 @@
+package top.umeh;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
+        return application.sources (UmehServerApplication.class);
+    }
+
+}
---
+++ src/main/java/top/umeh/UmehServerApplication.java
@@ -0,0 +1,13 @@
+package top.umeh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmehServerApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run (UmehServerApplication.class, args);
+    }
+
+}
