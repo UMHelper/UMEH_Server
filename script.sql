b6b32be84ad9fbd3e7d94f495c7ab4374eef4b5c
boxmars <2803726925@qq.com>
2021-12-19 03:38:56 +0800

    database arch

--- script.sql
+++ script.sql
@@ -1,73 +1,110 @@
 create table if not exists umeh.course
 (
-    Offering_Unit         char          null,
-    Offering_Department   char          null,
-    New_code              char          not null
+    offering_unit         char(255)     null,
+    offering_department   char(255)     null,
+    new_code              char(255)     not null
         constraint `PRIMARY`
         primary key,
-    Old_code              char          null,
-    courseTitleEng        char          null,
-    courseTitleChi        char          null,
-    Credits               int default 3 null,
-    Medium_of_Instruction char          null,
+    old_code              char(255)     null,
+    course_title_eng      char(255)     null,
+    course_title_chi      char(255)     null,
+    credits               int default 3 null,
+    course_duration       char(255)     null,
+    medium_of_instruction char(255)     null,
     constraint course_New_code_uindex
-        unique (New_code)
+        unique (new_code)
 );
 
 create table if not exists umeh.prof
 (
-    name char not null
+    name char(255) not null
         constraint `PRIMARY`
         primary key,
     constraint prof_name_uindex
         unique (name)
 );
 
+create table if not exists umeh.statistics
+(
+    name    char(255) not null
+        constraint `PRIMARY`
+        primary key,
+    course  int       null,
+    comment int       null,
+    constraint statistics_name_uindex
+        unique (name)
+);
+
 create table if not exists umeh.teach
 (
-    result     float null,
-    comments   int   null,
-    attendance float null,
-    grade      float null,
-    hard       float null,
-    reward     float null,
-    course     char  null,
-    prof       char  null,
     id         int auto_increment
         constraint `PRIMARY`
         primary key,
+    course     char(255) null,
+    prof       char(255) null,
+    result     float     null,
+    comments   int       null,
+    attendance float     null,
+    grade      float     null,
+    hard       float     null,
+    reward     float     null,
     constraint teach_id_uindex
         unique (id),
     constraint course
-        foreign key (course) references umeh.course (New_code)
-            on update set null on delete set null,
+        foreign key (course) references umeh.course (new_code),
     constraint prof
         foreign key (prof) references umeh.prof (name)
-            on update set null on delete set null
 );
 
 create table if not exists umeh.comment
 (
-    teach      int      null,
-    content    longtext null,
-    attendance float    null,
-    pre        float    null,
-    grade      float    null,
-    hard       float    null,
-    reward     float    null,
-    recommend  float    null,
-    assignment float    null,
-    result     float    null,
-    pub_time   datetime null,
-    upvote     int      null,
-    downvote   int      null,
     id         int auto_increment
         constraint `PRIMARY`
         primary key,
+    content    longtext      null,
+    attendance float         null,
+    pre        float         null,
+    grade      float         null,
+    hard       float         null,
+    reward     float         null,
+    recommend  float         null,
+    assignment float         null,
+    result     float         null,
+    pub_time   datetime      null,
+    upvote     int default 0 null,
+    downvote   int default 0 null,
+    teach      int           not null,
     constraint comment_id_uindex
         unique (id),
-    constraint teach
+    constraint teaach
         foreign key (teach) references umeh.teach (id)
+);
+
+create table if not exists umeh.time_location
+(
+    id       int auto_increment
+        constraint `PRIMARY`
+        primary key,
+    date     char(255) null,
+    time     char(255) null,
+    location char(255) null,
+    constraint time_location_id_uindex
+        unique (id)
+);
+
+create table if not exists umeh.schedule
+(
+    id            int auto_increment
+        constraint `PRIMARY`
+        primary key,
+    teach         int null,
+    time_location int null,
+    constraint schedule_id_uindex
+        unique (id),
+    constraint schedule_teach
+        foreign key (teach) references umeh.teach (id),
+    constraint schedule_time_location
+        foreign key (time_location) references umeh.time_location (id)
 );
 
 
